329bcdda6e3bc6a9b4164946df8a247b6c42dcf7
Cédric Foellmi <cedric@onekilopars.ec>
2015-03-25 14:39:50 +0100

    Initial commit

---
+++ RTSAnalytics/RTSAnalytics.h
@@ -0,0 +1,13 @@
+//
+//  RTSAnalytics.h
+//  RTSAnalytics
+//
+//  Created by Cédric Foellmi on 25/03/15.
+//  Copyright (c) 2015 RTS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RTSAnalytics : NSObject
+
+@end
---
+++ RTSAnalytics/RTSAnalytics.m
@@ -0,0 +1,13 @@
+//
+//  RTSAnalytics.m
+//  RTSAnalytics
+//
+//  Created by Cédric Foellmi on 25/03/15.
+//  Copyright (c) 2015 RTS. All rights reserved.
+//
+
+#import "RTSAnalytics.h"
+
+@implementation RTSAnalytics
+
+@end
