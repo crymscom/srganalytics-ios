b02842b5aa99e3e239a2a9621ba475d00a5311fb
Cédric Foellmi <cedric@onekilopars.ec>
2015-03-31 12:07:26 +0200

    Adding global header

---
+++ RTSAnalytics/RTSAnalytics.h
@@ -0,0 +1,16 @@
+//
+//  RTSAnalytics.h
+//  RTSAnalytics
+//
+//  Created by Cédric Foellmi on 31/03/15.
+//  Copyright (c) 2015 RTS. All rights reserved.
+//
+
+#ifndef RTSAnalytics_RTSAnalytics_h
+#define RTSAnalytics_RTSAnalytics_h
+
+#import "RTSAnalyticsTracker.h"
+#import "RTSAnalyticsTrackerConfig.h"
+#import "RTSAnalyticsDataSource.h"
+
+#endif
