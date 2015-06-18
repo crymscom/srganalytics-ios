7d60508e6da635ecb5a3a72a677857a346faed77
Samuel Défago <defagos@gmail.com>
2015-06-18 14:37:51 +0200

    Use built-in macros

--- RTSAnalytics/Core/RTSAnalyticsVersion.m
+++ RTSAnalytics/Core/RTSAnalyticsVersion.m
@@ -6,15 +6,12 @@
 //  Copyright (c) 2015 RTS. All rights reserved.
 //
 
-#define RTSStringize_(x) #x
-#define RTSStringize(x) RTSStringize_(x)
-
 #import "RTSAnalyticsVersion_private.h"
 
 NSString * const RTSAnalyticsVersion(void)
 {
 #ifdef RTS_ANALYTICS_VERSION
-    return @(RTSStringize(RTS_ANALYTICS_VERSION));
+    return @(OS_STRINGIFY(RTS_ANALYTICS_VERSION));
 #else
     #warning No explicit version has been specified, set to "dev". Compile the project with a preprocessor macro called RTS_ANALYTICS_VERSION supplying the version number (without quotes)
     return @"dev";
