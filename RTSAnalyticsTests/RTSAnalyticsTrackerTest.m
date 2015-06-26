51b9dd7946ea0805c0f8b9fe4364847a8110bf73
Cédric Luthi <cedric.luthi@rts.ch>
2015-06-26 16:59:41 +0200

    Remove the global push notification state
    Give responsibility to the application to tell if the view was presented as a result of a push notification.

--- RTSAnalyticsTests/RTSAnalyticsTrackerTest.m
+++ RTSAnalyticsTests/RTSAnalyticsTrackerTest.m
@@ -39,7 +39,7 @@
     id comScoreClassMock = OCMClassMock([CSComScore class]);
     
     // Also check that when we have multiple trackers, the setup is done only once for comScore.
-	[[RTSAnalyticsTracker sharedTracker] startTrackingForBusinessUnit:SSRBusinessUnitRTS launchOptions:nil mediaDataSource:self.dataSourceMock];
+	[[RTSAnalyticsTracker sharedTracker] startTrackingForBusinessUnit:SSRBusinessUnitRTS mediaDataSource:self.dataSourceMock];
 	
     OCMVerify([comScoreClassMock setCustomerC2:[OCMArg isNotNil]]);
     OCMVerify([comScoreClassMock setPublisherSecret:[OCMArg isNotNil]]);
