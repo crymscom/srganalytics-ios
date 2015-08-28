794422212e57c55611921353f84018ae849c8349
Cédric Luthi <cedric.luthi@rts.ch>
2015-08-28 13:53:07 +0200

    Document Netmetrix notifications

--- RTSAnalytics/Core/RTSAnalyticsNetmetrixTracker.h
+++ RTSAnalytics/Core/RTSAnalyticsNetmetrixTracker.h
@@ -12,9 +12,21 @@
  */
 
 /**
- *  Posted when the request's response is received
+ *  Posted when the request's response is received. The `object` of the notification is a NSURLRequest.
  */
 FOUNDATION_EXTERN NSString * const RTSAnalyticsNetmetrixRequestDidFinishNotification;
+
+/**
+ * A NSNumber (boolean) indicating success in the user info dictionary of `RTSAnalyticsNetmetrixRequestDidFinishNotification`.
+ */
 FOUNDATION_EXTERN NSString * const RTSAnalyticsNetmetrixRequestSuccessUserInfoKey;
+
+/**
+ *  A NSError in the user info dictionary of `RTSAnalyticsNetmetrixRequestDidFinishNotification`. This key is not present if the request succeeded.
+ */
 FOUNDATION_EXTERN NSString * const RTSAnalyticsNetmetrixRequestErrorUserInfoKey;
+
+/**
+ *  A NSURLResponse in the user info dictionary of `RTSAnalyticsNetmetrixRequestDidFinishNotification`.
+ */
 FOUNDATION_EXTERN NSString * const RTSAnalyticsNetmetrixRequestResponseUserInfoKey;
