79d46171663d1c99241bda5b6deba10deb3e894c
Samuel Défago <defagos@gmail.com>
2015-11-19 14:37:31 +0100

    Replace compiler warning with log message

--- RTSAnalytics/Core/RTSAnalyticsVersion.m
+++ RTSAnalytics/Core/RTSAnalyticsVersion.m
@@ -6,12 +6,14 @@
 
 #import "RTSAnalyticsVersion_private.h"
 
+#import "RTSAnalyticsLogger.h"
+
 NSString * const RTSAnalyticsVersion(void)
 {
 #ifdef RTS_ANALYTICS_VERSION
     return @(OS_STRINGIFY(RTS_ANALYTICS_VERSION));
 #else
-    #warning No explicit version has been specified, set to "dev". Compile the project with a preprocessor macro called RTS_ANALYTICS_VERSION supplying the version number (without quotes)
+    RTSAnalyticsLogWarning(@"No explicit version has been specified, set to 'dev'. Compile the project with a preprocessor macro called RTS_ANALYTICS_VERSION supplying the version number (without quotes)");
     return @"dev";
 #endif
 }
