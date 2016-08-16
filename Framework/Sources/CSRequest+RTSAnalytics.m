11ea34863b189e1173e99469535939f5a527ba73
Samuel Défago <defagos@gmail.com>
2016-08-16 09:16:21 +0200

    Update label extraction to match comScore library internal changes

--- Framework/Sources/CSRequest+RTSAnalytics.m
+++ Framework/Sources/CSRequest+RTSAnalytics.m
@@ -11,32 +11,6 @@ NSString * const RTSAnalyticsComScoreRequestDidFinishNotification = @"RTSAnalyti
 NSString * const RTSAnalyticsComScoreRequestSuccessUserInfoKey = @"RTSAnalyticsSuccess";
 NSString * const RTSAnalyticsComScoreRequestLabelsUserInfoKey = @"RTSAnalyticsLabels";
 
-static NSDictionary *RTSDictionaryFromURLEncodedStringWithEncoding(NSString *URLEncodedString, NSStringEncoding encoding)
-{
-    NSMutableDictionary *queryComponents = [NSMutableDictionary dictionary];
-    for (NSString *keyValuePairString in [URLEncodedString componentsSeparatedByString:@"&"]) {
-        NSArray *keyValuePairArray = [keyValuePairString componentsSeparatedByString:@"="];
-        if ([keyValuePairArray count] < 2) continue; // Verify that there is at least one key, and at least one value.  Ignore extra = signs
-        NSString *key = [[keyValuePairArray objectAtIndex:0] stringByReplacingPercentEscapesUsingEncoding:encoding];
-        NSString *value = [[keyValuePairArray objectAtIndex:1] stringByReplacingPercentEscapesUsingEncoding:encoding];
-        
-            // URL spec says that multiple values are allowed per key
-        id results = [queryComponents objectForKey:key];
-        if (results) {
-            if ([results isKindOfClass:[NSMutableArray class]]) {
-                [(NSMutableArray *)results addObject:value];
-            } else {
-                    // On second occurrence of the key, convert into an array
-                NSMutableArray *values = [NSMutableArray arrayWithObjects:results, value, nil];
-                [queryComponents setObject:values forKey:key];
-            }
-        } else {
-            [queryComponents setObject:value forKey:key];
-        }
-    }
-    return queryComponents;
-}
-
 @implementation CSRequest (RTSNotification)
 
 static BOOL (*sendIMP)(CSRequest *, SEL);
@@ -46,11 +20,19 @@ static BOOL NotificationSend(CSRequest *self, SEL _cmd)
 {
 	BOOL success = sendIMP(self, _cmd);
 	
-	NSURLRequest *request = [self valueForKey:@"request"];
-	NSDictionary *labels = RTSDictionaryFromURLEncodedStringWithEncoding(request.URL.query, NSUTF8StringEncoding);
-	NSDictionary *userInfo = @{ RTSAnalyticsComScoreRequestSuccessUserInfoKey: @(success), RTSAnalyticsComScoreRequestLabelsUserInfoKey: labels };
+    // Fragile, might break with future versions of the comScore library
+    id measurement = [self valueForKey:@"measurement"];
+    NSDictionary *labelsMap = object_getIvar(measurement, class_getInstanceVariable([measurement class], "_labelsMap"));
     
-    [[NSNotificationCenter defaultCenter] postNotificationName:RTSAnalyticsComScoreRequestDidFinishNotification object:request userInfo:userInfo];
+    NSMutableDictionary *labels = [NSMutableDictionary dictionary];
+    for (id label in labelsMap.allValues) {
+        NSString *name = [label valueForKey:@"name"];
+        NSString *value = [label valueForKey:@"value"];
+        labels[name] = value;
+    }
+    
+	NSDictionary *userInfo = @{ RTSAnalyticsComScoreRequestSuccessUserInfoKey: @(success), RTSAnalyticsComScoreRequestLabelsUserInfoKey: [labels copy] };
+    [[NSNotificationCenter defaultCenter] postNotificationName:RTSAnalyticsComScoreRequestDidFinishNotification object:self userInfo:userInfo];
 	
 	return success;
 }
