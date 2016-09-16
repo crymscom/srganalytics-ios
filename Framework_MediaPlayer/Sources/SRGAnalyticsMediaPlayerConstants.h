00082880ad135f9496f708f6723085839d7de28c
Samuel Défago <defagos@gmail.com>
2016-09-16 18:46:29 +0200

    Add TODO

--- Framework_MediaPlayer/Sources/SRGAnalyticsMediaPlayerConstants.h
+++ Framework_MediaPlayer/Sources/SRGAnalyticsMediaPlayerConstants.h
@@ -9,6 +9,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 // Analytics available in the current user info dicitonnary
+// TODO: Remove this header and its implementation file, hide this constant elsewhere
 OBJC_EXTERN NSString * const SRGAnalyticsMediaPlayerDictionnaryKey;        // A dictionnary of analytics keys
 
 NS_ASSUME_NONNULL_END
