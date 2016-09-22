40e20b34dc4db6f157628c5b7d54410327a51a53
Samuel Défago <defagos@gmail.com>
2016-09-22 09:56:53 +0200

    Add test stubs

---
+++ Tests/Sources/TrackerTestCase.m
@@ -0,0 +1,34 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TrackerTestCase : XCTestCase
+
+@end
+
+@implementation TrackerTestCase
+
+#pragma mark Tests
+
+// TODO: Move tracker initialization outside tests (since it is a singleton)
+
+- (void)testStart
+{
+
+}
+
+- (void)testHiddenEvent
+{
+    XCTFail(@"TODO");
+}
+
+- (void)testHiddenEventWithNoName
+{
+    XCTFail(@"TODO");
+}
+
+@end
