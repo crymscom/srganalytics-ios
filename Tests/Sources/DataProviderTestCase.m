42c9659d26bef7887816fa29cebfbc8343a5afd2
Samuel Défago <defagos@gmail.com>
2016-09-29 13:19:48 +0200

    Add test file for data provider related framework

---
+++ Tests/Sources/DataProviderTestCase.m
@@ -0,0 +1,22 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DataProviderTestCase : XCTestCase
+
+@end
+
+@implementation DataProviderTestCase
+
+#pragma mark Tests
+
+- (void)testPrepareToPlayMediaComposition
+{
+    
+}
+
+@end
