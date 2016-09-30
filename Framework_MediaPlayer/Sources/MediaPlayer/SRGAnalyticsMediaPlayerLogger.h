29c922b264382571bd0517e64f5355b6880ef1ce
Samuel Défago <defagos@gmail.com>
2016-09-30 18:22:14 +0200

    Add logging to the main analytics library

---
+++ Framework_MediaPlayer/Sources/MediaPlayer/SRGAnalyticsMediaPlayerLogger.h
@@ -0,0 +1,16 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <SRGLogger/SRGLogger.h>
+
+/**
+ *  Helper macros for logging
+ */
+#define SRGAnalyticsMediaPlayerLogVerbose(category, format, ...) SRGLogVerbose(@"ch.srgssr.analytics.mediaplayer", category, format, ##__VA_ARGS__)
+#define SRGAnalyticsMediaPlayerLogVerbose(category, format, ...)   SRGLogDebug(@"ch.srgssr.analytics.mediaplayer", category, format, ##__VA_ARGS__)
+#define SRGAnalyticsMediaPlayerLogVerbose(category, format, ...)    SRGLogInfo(@"ch.srgssr.analytics.mediaplayer", category, format, ##__VA_ARGS__)
+#define SRGAnalyticsMediaPlayerLogVerbose(category, format, ...) SRGLogWarning(@"ch.srgssr.analytics.mediaplayer", category, format, ##__VA_ARGS__)
+#define SRGAnalyticsMediaPlayerLogVerbose(category, format, ...)   SRGLogError(@"ch.srgssr.analytics.mediaplayer", category, format, ##__VA_ARGS__)
