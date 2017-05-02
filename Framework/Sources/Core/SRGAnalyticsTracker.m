88eefd58f6009d52cacc09cf2073d4118cb81de4
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2017-05-02 11:51:18 +0200

    Set TagCommander HIT_TYPE for event

--- Framework/Sources/Core/SRGAnalyticsTracker.m
+++ Framework/Sources/Core/SRGAnalyticsTracker.m
@@ -241,7 +241,8 @@ SRGAnalyticsBusinessUnitIdentifier const SRGAnalyticsBusinessUnitIdentifierTEST 
 {
     NSAssert(title.length != 0, @"A title is required");
     
-    [self.tagCommander addData:@"TITLE" withValue:title];
+    [self.tagCommander addData:@"EVENT_NAME" withValue:title];
+    [self.tagCommander addData:@"HIT_TYPE" withValue:@"click"];
     [customLabels enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull key, NSString * _Nonnull object, BOOL * _Nonnull stop) {
         [self.tagCommander addData:key withValue:object];
     }];
