9350570bcc960153b37c9b175df44979960c809d
Samuel Défago <defagos@gmail.com>
2017-06-29 16:30:51 +0200

    Update protocol for segments analytics

--- Framework_DataProvider/SRGAnalytics_DataProvider.h
+++ Framework_DataProvider/SRGAnalytics_DataProvider.h
@@ -11,4 +11,4 @@
 
 // Public headers.
 #import "SRGMediaPlayerController+SRGAnalytics_DataProvider.h"
-#import "SRGSegment+SRGAnalytics_DataProvider.h"
+#import "SRGSubdivision+SRGAnalytics_DataProvider.h"
