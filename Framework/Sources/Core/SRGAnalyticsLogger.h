1cc783cacfd719f586a3bdb49803b9b88e057a6c
Samuel Défago <defagos@gmail.com>
2017-08-03 06:21:24 +0200

    Fix incorrect logger macros

--- Framework/Sources/Core/SRGAnalyticsLogger.h
+++ Framework/Sources/Core/SRGAnalyticsLogger.h
@@ -9,8 +9,8 @@
 /**
  *  Helper macros for logging.
  */
-#define SRGAnalyticsLogVerbose(category, format, ...) SRGLogVerbose(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
-#define SRGAnalyticsLogDebug(category, format, ...)   SRGLogDebug(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
-#define SRGAnalyticsLogInfo(category, format, ...)    SRGLogInfo(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
-#define SRGAnalyticsLogWarning(category, format, ...) SRGLogWarning(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
-#define SRGAnalyticsLogError(category, format, ...)   SRGLogError(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
+#define SRGAnalyticsLogVerbose(category, format, ...)   SRGLogVerbose(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
+#define SRGAnalyticsLogDebug(category, format, ...)     SRGLogDebug(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
+#define SRGAnalyticsLogInfo(category, format, ...)      SRGLogInfo(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
+#define SRGAnalyticsLogWarning(category, format, ...)   SRGLogWarning(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
+#define SRGAnalyticsLogError(category, format, ...)     SRGLogError(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
--- Framework_DataProvider/Sources/SRGAnalyticsDataProvider.h
+++ Framework_DataProvider/Sources/SRGAnalyticsDataProvider.h
@@ -10,7 +10,7 @@
  *  Helper macros for logging.
  */
 #define SRGAnalyticsDataProviderLogVerbose(category, format, ...) SRGLogVerbose(@"ch.srgssr.analytics.dataprovider", category, format, ##__VA_ARGS__)
-#define SRGAnalyticsDataProviderLogVerbose(category, format, ...)   SRGLogDebug(@"ch.srgssr.analytics.dataprovider", category, format, ##__VA_ARGS__)
-#define SRGAnalyticsDataProviderLogVerbose(category, format, ...)    SRGLogInfo(@"ch.srgssr.analytics.dataprovider", category, format, ##__VA_ARGS__)
+#define SRGAnalyticsDataProviderLogVerbose(category, format, ...) SRGLogDebug(@"ch.srgssr.analytics.dataprovider", category, format, ##__VA_ARGS__)
+#define SRGAnalyticsDataProviderLogVerbose(category, format, ...) SRGLogInfo(@"ch.srgssr.analytics.dataprovider", category, format, ##__VA_ARGS__)
 #define SRGAnalyticsDataProviderLogVerbose(category, format, ...) SRGLogWarning(@"ch.srgssr.analytics.dataprovider", category, format, ##__VA_ARGS__)
-#define SRGAnalyticsDataProviderLogVerbose(category, format, ...)   SRGLogError(@"ch.srgssr.analytics.dataprovider", category, format, ##__VA_ARGS__)
+#define SRGAnalyticsDataProviderLogVerbose(category, format, ...) SRGLogError(@"ch.srgssr.analytics.dataprovider", category, format, ##__VA_ARGS__)
