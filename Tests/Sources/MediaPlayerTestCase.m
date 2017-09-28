1629eb64ac75bc6a5c751bc787d53cc96783e22d
Samuel Défago <defagos@gmail.com>
2017-09-28 14:59:28 +0200

    Use working HTTP Apple stream URLs

--- Tests/Sources/MediaPlayerTestCase.m
+++ Tests/Sources/MediaPlayerTestCase.m
@@ -14,7 +14,7 @@ typedef BOOL (^EventExpectationHandler)(NSString *event, NSDictionary *labels);
 
 static NSURL *OnDemandTestURL(void)
 {
-    return [NSURL URLWithString:@"https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8"];
+    return [NSURL URLWithString:@"http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8"];
 }
 
 static NSURL *LiveTestURL(void)
