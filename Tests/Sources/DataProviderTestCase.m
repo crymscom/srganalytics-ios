673ab82df25a54ce5687d9587927cdb0ce159484
Samuel Défago <defagos@gmail.com>
2017-10-09 13:03:23 +0200

    Test metadata updates

--- Tests/Sources/DataProviderTestCase.m
+++ Tests/Sources/DataProviderTestCase.m
@@ -13,6 +13,11 @@ static NSURL *ServiceTestURL(void)
     return [NSURL URLWithString:@"http://il.srgssr.ch"];
 }
 
+static NSURL *MMFTestURL(void)
+{
+    return [NSURL URLWithString:@"http://play-mmf.herokuapp.com"];
+}
+
 @interface DataProviderTestCase : AnalyticsTestCase
 
 @property (nonatomic) SRGMediaPlayerController *mediaPlayerController;
@@ -158,19 +163,131 @@ static NSURL *ServiceTestURL(void)
     [self waitForExpectationsWithTimeout:20. handler:nil];
 }
 
+- (void)testUpdateWithCompatibleMediaComposition
+{
+    XCTestExpectation *expectation1 = [self expectationWithDescription:@"Play"];
+    
+    __block NSString *originalTitle = nil;
+    SRGDataProvider *dataProvider = [[SRGDataProvider alloc] initWithServiceURL:MMFTestURL() businessUnitIdentifier:SRGDataProviderBusinessUnitIdentifierRTS];
+    NSDate *startDate = [NSDate date];
+    NSDate *endDate = [startDate dateByAddingTimeInterval:200];
+    NSString *URNString = [NSString stringWithFormat:@"urn:rts:video:_bipbop_advanced_delay_%@_%@", @((NSInteger)[startDate timeIntervalSince1970]), @((NSInteger)[endDate timeIntervalSince1970])];
+    SRGMediaURN *URN = [SRGMediaURN mediaURNWithString:URNString];
+    [[dataProvider mediaCompositionWithURN:URN chaptersOnly:NO completionBlock:^(SRGMediaComposition * _Nullable mediaComposition, NSError * _Nullable error) {
+        XCTAssertNotNil(mediaComposition);
+        originalTitle = mediaComposition.mainChapter.title;
+        
+        [self.mediaPlayerController playMediaComposition:mediaComposition withPreferredStreamingMethod:SRGStreamingMethodNone quality:SRGQualityHD startBitRate:0 userInfo:nil resume:YES completionHandler:^(NSError * _Nonnull error) {
+            [expectation1 fulfill];
+        }];
+    }] resume];
+    
+    [self waitForExpectationsWithTimeout:20. handler:nil];
+    
+    // The test media title changes over time. Wait a little bit to detect a change.
+    [self expectationForElapsedTimeInterval:2. withHandler:nil];
+    [self waitForExpectationsWithTimeout:20. handler:nil];
+    
+    XCTestExpectation *expectation2 = [self expectationWithDescription:@"Update"];
+    
+    [[dataProvider mediaCompositionWithURN:URN chaptersOnly:NO completionBlock:^(SRGMediaComposition * _Nullable mediaComposition, NSError * _Nullable error) {
+        XCTAssertNotNil(mediaComposition);
+        
+        self.mediaPlayerController.mediaComposition = mediaComposition;
+        [expectation2 fulfill];
+    }] resume];
+    
+    [self waitForExpectationsWithTimeout:20. handler:nil];
+    
+    XCTAssertNotEqualObjects(self.mediaPlayerController.mediaComposition.mainChapter.title, originalTitle);
+}
+
 - (void)testUpdateWithoutMediaComposition
 {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Play"];
     
+    __block SRGMediaComposition *fetchedMediaComposition = nil;
+    SRGDataProvider *dataProvider = [[SRGDataProvider alloc] initWithServiceURL:ServiceTestURL() businessUnitIdentifier:SRGDataProviderBusinessUnitIdentifierSRF];
+    [[dataProvider videoMediaCompositionWithUid:@"2b1bf779-6442-4f78-969e-2194020ae263" chaptersOnly:NO completionBlock:^(SRGMediaComposition * _Nullable mediaComposition, NSError * _Nullable error) {
+        XCTAssertNotNil(mediaComposition);
+        fetchedMediaComposition = mediaComposition;
+        
+        [self.mediaPlayerController playMediaComposition:mediaComposition withPreferredStreamingMethod:SRGStreamingMethodNone quality:SRGQualityHD startBitRate:0 userInfo:nil resume:YES completionHandler:^(NSError * _Nonnull error) {
+            [expectation fulfill];
+        }];
+    }] resume];
+    
+    [self waitForExpectationsWithTimeout:20. handler:nil];
+    
+    self.mediaPlayerController.mediaComposition = nil;
+    XCTAssertEqualObjects(self.mediaPlayerController.mediaComposition, fetchedMediaComposition);
 }
 
 - (void)testMediaCompositionUpdateWithDifferentChapter
 {
+    XCTestExpectation *expectation1 = [self expectationWithDescription:@"Play"];
     
+    __block SRGMediaComposition *mediaComposition1 = nil;
+    SRGDataProvider *dataProvider = [[SRGDataProvider alloc] initWithServiceURL:ServiceTestURL() businessUnitIdentifier:SRGDataProviderBusinessUnitIdentifierSRF];
+    [[dataProvider videoMediaCompositionWithUid:@"c4927fcf-e1a0-0001-7edd-1ef01d441651" chaptersOnly:NO completionBlock:^(SRGMediaComposition * _Nullable mediaComposition, NSError * _Nullable error) {
+        XCTAssertNotNil(mediaComposition);
+        mediaComposition1 = mediaComposition;
+        
+        [self.mediaPlayerController playMediaComposition:mediaComposition withPreferredStreamingMethod:SRGStreamingMethodNone quality:SRGQualityHD startBitRate:0 userInfo:nil resume:YES completionHandler:^(NSError * _Nonnull error) {
+            [expectation1 fulfill];
+        }];
+    }] resume];
+    
+    [self waitForExpectationsWithTimeout:20. handler:nil];
+    
+    XCTestExpectation *expectation2 = [self expectationWithDescription:@"Update"];
+    
+    [[dataProvider videoMediaCompositionWithUid:@"895b9096-f07d-4daa-83e0-ac6486ac72e3" chaptersOnly:NO completionBlock:^(SRGMediaComposition * _Nullable mediaComposition, NSError * _Nullable error) {
+        XCTAssertNotNil(mediaComposition);
+        
+        self.mediaPlayerController.mediaComposition = mediaComposition;
+        
+        // Incompatible media composition. No update must have taken place
+        XCTAssertEqualObjects(self.mediaPlayerController.mediaComposition, mediaComposition1);
+        
+        [expectation2 fulfill];
+    }] resume];
+    
+    [self waitForExpectationsWithTimeout:20. handler:nil];
 }
 
 - (void)testMediaCompositionUpdateWithDifferentSegment
 {
+    // Retrieve two media compositions of segments belonging to the same media composition
+    XCTestExpectation *expectation1 = [self expectationWithDescription:@"Play"];
     
+    __block SRGMediaComposition *mediaComposition1 = nil;
+    SRGDataProvider *dataProvider = [[SRGDataProvider alloc] initWithServiceURL:ServiceTestURL() businessUnitIdentifier:SRGDataProviderBusinessUnitIdentifierSRF];
+    [[dataProvider videoMediaCompositionWithUid:@"2b1bf779-6442-4f78-969e-2194020ae263" chaptersOnly:NO completionBlock:^(SRGMediaComposition * _Nullable mediaComposition, NSError * _Nullable error) {
+        XCTAssertNotNil(mediaComposition);
+        mediaComposition1 = mediaComposition;
+        
+        [self.mediaPlayerController playMediaComposition:mediaComposition withPreferredStreamingMethod:SRGStreamingMethodNone quality:SRGQualityHD startBitRate:0 userInfo:nil resume:YES completionHandler:^(NSError * _Nonnull error) {
+            [expectation1 fulfill];
+        }];
+    }] resume];
+    
+    [self waitForExpectationsWithTimeout:20. handler:nil];
+    
+    XCTestExpectation *expectation2 = [self expectationWithDescription:@"Update"];
+    
+    [[dataProvider videoMediaCompositionWithUid:@"197d52e6-7836-45e0-92f1-74862cc0873a" chaptersOnly:NO completionBlock:^(SRGMediaComposition * _Nullable mediaComposition, NSError * _Nullable error) {
+        XCTAssertNotNil(mediaComposition);
+        
+        self.mediaPlayerController.mediaComposition = mediaComposition;
+        
+        // Incompatible media composition. No update must have taken place
+        XCTAssertEqualObjects(self.mediaPlayerController.mediaComposition, mediaComposition1);
+        
+        [expectation2 fulfill];
+    }] resume];
+    
+    [self waitForExpectationsWithTimeout:20. handler:nil];
 }
 
 @end
