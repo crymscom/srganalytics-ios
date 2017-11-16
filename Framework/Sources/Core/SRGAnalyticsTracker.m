57335d68b1258195e294bc90d6abf8e01f4bfd68
Samuel Défago <defagos@gmail.com>
2017-11-16 20:16:01 +0100

    Remove page id from TagCommander data

--- Framework/Sources/Core/SRGAnalyticsTracker.m
+++ Framework/Sources/Core/SRGAnalyticsTracker.m
@@ -262,7 +262,6 @@ __attribute__((constructor)) static void SRGAnalyticsTrackerInit(void)
     [fullLabelsDictionary srg_safelySetString:title forKey:@"content_title"];
     [fullLabelsDictionary srg_safelySetString:self.configuration.businessUnitIdentifier.uppercaseString forKey:@"navigation_bu_distributer"];
     [fullLabelsDictionary srg_safelySetString:fromPushNotification ? @"true" : @"false" forKey:@"accessed_after_push_notification"];
-    [fullLabelsDictionary srg_safelySetString:[self pageIdWithTitle:title levels:levels] forKey:@"page_id"];
 
     [levels enumerateObjectsUsingBlock:^(NSString * _Nonnull object, NSUInteger idx, BOOL * _Nonnull stop) {
         if (idx > 7) {
