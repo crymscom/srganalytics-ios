12d38f8bd88770fba87cd54ee5d10c634d9db34b
Samuel Défago <defagos@gmail.com>
2018-03-19 09:19:37 +0100

    Send Akamai media analytics with player events

--- Framework_MediaPlayer/Sources/Helpers/SRGAkamaiAnalyticsTracker.m
+++ Framework_MediaPlayer/Sources/Helpers/SRGAkamaiAnalyticsTracker.m
@@ -9,11 +9,42 @@
 #import "NSBundle+SRGAnalytics_MediaPlayer.h"
 
 #import <AkamaiMediaAnalytics/AkamaiMediaAnalytics.h>
-#import <UIKit/UIKit.h>
+#import <SRGMediaPlayer/SRGMediaPlayer.h>
 
 @implementation SRGAkamaiAnalyticsTracker
 
 #pragma mark Notifications
+
++ (void)playbackStateDidChange:(NSNotification *)notification
+{
+    SRGMediaPlayerPlaybackState playbackState = [notification.userInfo[SRGMediaPlayerPlaybackStateKey] integerValue];
+    switch (playbackState) {
+        case SRGMediaPlayerPlaybackStatePreparing: {
+            SRGMediaPlayerController *mediaPlayerController = notification.object;
+            [AKAMMediaAnalytics_Av processWithAVPlayer:mediaPlayerController.player];
+            break;
+        }
+            
+        case SRGMediaPlayerPlaybackStateSeeking: {
+            [AKAMMediaAnalytics_Av beginScrub];
+            break;
+        }
+            
+        case SRGMediaPlayerPlaybackStateIdle: {
+            [AKAMMediaAnalytics_Av AVPlayerPlaybackCompleted];
+            break;
+        }
+            
+        default: {
+            break;
+        }
+    }
+    
+    SRGMediaPlayerPlaybackState previousPlaybackState = [notification.userInfo[SRGMediaPlayerPreviousPlaybackStateKey] integerValue];
+    if (previousPlaybackState == SRGMediaPlayerPlaybackStateSeeking) {
+        [AKAMMediaAnalytics_Av endScrub];
+    }
+}
 
 + (void)applicationWillTerminate:(NSNotification *)notification
 {
@@ -27,7 +58,11 @@ __attribute__((constructor)) static void SRGAkamaiAnalyticsTrackerInit(void)
     // Akamai media analytics SDK initialization
     NSURL *akamaiConfigurationFileURL = [[NSBundle srg_analyticsMediaPlayerBundle] URLForResource:@"akamai-media-analytics-configuration" withExtension:@"xml"];
     [AKAMMediaAnalytics_Av initWithConfigURL:akamaiConfigurationFileURL];
-    
+
+    [[NSNotificationCenter defaultCenter] addObserver:[SRGAkamaiAnalyticsTracker class]
+                                             selector:@selector(playbackStateDidChange:)
+                                                 name:SRGMediaPlayerPlaybackStateDidChangeNotification
+                                               object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:[SRGAkamaiAnalyticsTracker class]
                                              selector:@selector(applicationWillTerminate:)
                                                  name:UIApplicationWillTerminateNotification
