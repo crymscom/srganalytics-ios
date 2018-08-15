bce75709505862cc8ee8793fa2b951784ab71a31
Samuel Défago <defagos@gmail.com>
2018-08-15 13:49:03 +0200

    Only apply bitrate for HLS Akamai playlists

--- Framework_DataProvider/Sources/SRGMediaComposition+SRGAnalytics_DataProvider.m
+++ Framework_DataProvider/Sources/SRGMediaComposition+SRGAnalytics_DataProvider.m
@@ -160,10 +160,10 @@
         return NO;
     }
     
-    // Use the preferrred start bit rate is set. Currrently only supported by Akamai via a __b__ parameter (the actual
-    // bitrate will be rounded to the nearest available quality)
+    // Use the preferrred start bit rate is set. Currrently only supported for HLS streams by Akamai, via a __b__ parameter
+    // (the actual bitrate will be rounded to the nearest available quality)
     NSURL *URL = resource.URL;
-    if (startBitRate != 0 && [URL.host containsString:@"akamai"]) {
+    if (startBitRate != 0 && [URL.host containsString:@"akamai"] && [URL.absoluteString.pathExtension isEqualToString:@"m3u8"]) {
         NSURLComponents *URLComponents = [NSURLComponents componentsWithURL:URL resolvingAgainstBaseURL:NO];
         
         NSMutableArray<NSURLQueryItem *> *queryItems = [URLComponents.queryItems mutableCopy] ?: [NSMutableArray array];
