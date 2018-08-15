5ac5100df50ebf63ec9f6c724f42875c4a526290
Samuel Défago <defagos@gmail.com>
2018-08-15 07:32:48 +0200

    Rename variable for consistency

--- Framework/Sources/Helpers/NSBundle+SRGAnalytics.m
+++ Framework/Sources/Helpers/NSBundle+SRGAnalytics.m
@@ -15,8 +15,8 @@
 + (instancetype)srg_analyticsBundle
 {
     static NSBundle *s_bundle;
-    static dispatch_once_t s_once;
-    dispatch_once(&s_once, ^{
+    static dispatch_once_t s_onceToken;
+    dispatch_once(&s_onceToken, ^{
         NSString *bundlePath = [[NSBundle bundleForClass:[SRGAnalyticsTracker class]].bundlePath stringByAppendingPathComponent:@"SRGAnalytics.bundle"];
         s_bundle = [NSBundle bundleWithPath:bundlePath];
         NSAssert(s_bundle, @"Please add SRGAnalytics.bundle to your project resources");
