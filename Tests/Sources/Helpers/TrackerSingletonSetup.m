a3730bd78adf424d960fe69037682636476b0cbd
Samuel Défago <defagos@gmail.com>
2018-08-28 15:22:23 +0200

    Link SRG Content Protection at runtime with the test target

--- Tests/Sources/Helpers/TrackerSingletonSetup.m
+++ Tests/Sources/Helpers/TrackerSingletonSetup.m
@@ -4,11 +4,16 @@
 //  License information is available from the LICENSE file.
 //
 
+#import <dlfcn.h>
 #import <SRGAnalytics/SRGAnalytics.h>
 
 // The singleton can be only setup once. Do not perform in a test case setup
 __attribute__((constructor)) static void SetupTestSingletonTracker(void)
 {
+    // We cannot link the framework with the usual OTHER_LDFLAGS setting, the build would fail when building the open
+    // source version which has no access to the framework.
+    dlopen("SRGContentProtection.framework/SRGContentProtection", RTLD_LAZY);
+    
     SRGAnalyticsConfiguration *configuration = [[SRGAnalyticsConfiguration alloc] initWithBusinessUnitIdentifier:SRGAnalyticsBusinessUnitIdentifierRTS
                                                                                                        container:10
                                                                                              comScoreVirtualSite:@"rts-app-test-v"
