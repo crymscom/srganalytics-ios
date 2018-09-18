16d2f181d1d44d8702a3761a606189bb5c4863b4
Samuel Défago <defagos@gmail.com>
2018-09-18 07:52:04 +0200

    Revert "Ensure correct linkage and execution even when the content protection framework is missing"
    This reverts commit f5e59ecf2b6ce6339b5f999a9c0dec87f05e5887. The method still needs to be called loosely (-performSelector: or direct method invocation) for the dependency to be optional at link time

--- Framework/Sources/SRGAnalytics.m
+++ Framework/Sources/SRGAnalytics.m
@@ -8,30 +8,18 @@
 
 #import "NSBundle+SRGAnalytics.h"
 
-#if __has_include(<SRGContentProtection/SRGContentProtection.h>)
-#import <SRGContentProtection/SRGContentProtection.h>
-#endif
+// Default implementation if not linking against SRGContentProtection.framework
+FOUNDATION_EXPORT BOOL SRGContentProtectionIsPublic(void)
+{
+    return YES;
+}
 
 NSString *SRGAnalyticsMarketingVersion(void)
 {
     return [NSBundle srg_analyticsBundle].infoDictionary[@"CFBundleShortVersionString"];
 }
 
-@implementation NSBundle (SRGAnalyticsVersion)
-
-+ (BOOL)srg_analyticsIsPublic
+BOOL SRGAnalyticsIsPublic(void)
 {
-#if __has_include(<SRGContentProtection/SRGContentProtection.h>)
-    if ([[NSBundle class] respondsToSelector:@selector(srg_contentProtectionIsPublic)]) {
-        return [NSBundle srg_contentProtectionIsPublic];
-    }
-    else {
-        return YES;
-    }
-#else
-    return YES;
-#endif
+    return (&SRGContentProtectionIsPublic != NULL) ? SRGContentProtectionIsPublic() : YES;
 }
-
-@end
-
