4b85abe0a3e07a9159a2de3251b7b16cef756e5f
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2018-10-01 20:57:23 +0200

    Add unit tests for hidden event values

--- Tests/Sources/HiddenEventLabelsTestCase.m
+++ Tests/Sources/HiddenEventLabelsTestCase.m
@@ -25,11 +25,14 @@
     labels.type = @"type";
     labels.value = @"value";
     labels.source = @"source";
+    labels.values = @[ @"value1", @"value2" ];
     labels.customInfo = @{ @"key" : @"value" };
     
     NSDictionary *labelsDictionary = @{ @"event_type" : @"type",
                                         @"event_value" : @"value",
                                         @"event_source" : @"source",
+                                        @"event_value_1" : @"value1",
+                                        @"event_value_2" : @"value2",
                                         @"key" : @"value" };
     XCTAssertEqualObjects(labels.labelsDictionary, labelsDictionary);
 }
@@ -40,6 +43,7 @@
     labels1.type = @"type";
     labels1.value = @"value";
     labels1.source = @"source";
+    labels1.values = @[ @"value1", @"value2" ];
     labels1.customInfo = @{ @"key" : @"value" };
     XCTAssertEqualObjects(labels1, labels1);
     
@@ -47,6 +51,7 @@
     labels2.type = @"type";
     labels2.value = @"value";
     labels2.source = @"source";
+    labels2.values = @[ @"value1", @"value2" ];
     labels2.customInfo = @{ @"key" : @"value" };
     XCTAssertEqualObjects(labels1, labels2);
     
@@ -54,6 +59,7 @@
     labels3.type = @"other_type";
     labels3.value = @"value";
     labels3.source = @"source";
+    labels3.values = @[ @"value1", @"value2" ];
     labels3.customInfo = @{ @"key" : @"value" };
     XCTAssertNotEqualObjects(labels1, labels3);
     
@@ -61,6 +67,7 @@
     labels4.type = @"type";
     labels4.value = @"other_value";
     labels4.source = @"source";
+    labels4.values = @[ @"value1", @"value2" ];
     labels4.customInfo = @{ @"key" : @"value" };
     XCTAssertNotEqualObjects(labels1, labels4);
     
@@ -68,15 +75,27 @@
     labels5.type = @"type";
     labels5.value = @"value";
     labels5.source = @"other_source";
+    labels5.values = @[ @"value1", @"value2" ];
     labels5.customInfo = @{ @"key" : @"value" };
     XCTAssertNotEqualObjects(labels1, labels5);
     
     SRGAnalyticsHiddenEventLabels *labels6 = [[SRGAnalyticsHiddenEventLabels alloc] init];
     labels6.type = @"type";
     labels6.value = @"value";
+    labels6.values = @[ @"other_value1", @"value2" ];
     labels6.source = @"source";
-    labels6.customInfo = @{ @"other_key" : @"other_value" };
+    labels6.customInfo = @{ @"key" : @"value" };
     XCTAssertNotEqualObjects(labels1, labels6);
+    
+    SRGAnalyticsHiddenEventLabels *labels7 = [[SRGAnalyticsHiddenEventLabels alloc] init];
+    labels7.type = @"type";
+    labels7.value = @"value";
+    labels7.source = @"source";
+    labels7.values = @[ @"value1", @"value2" ];
+    labels7.customInfo = @{ @"other_key" : @"other_value" };
+    XCTAssertNotEqualObjects(labels1, labels7);
+    
+    
 }
 
 - (void)testCopy
@@ -85,6 +104,7 @@
     labels.type = @"type";
     labels.value = @"value";
     labels.source = @"source";
+    labels.values = @[ @"value1", @"value2" ];
     labels.customInfo = @{ @"key" : @"value" };
     
     SRGAnalyticsHiddenEventLabels *labelsCopy = [labels copy];
@@ -101,6 +121,33 @@
                            @"event_value" : @"overridden",
                            @"event_source" : @"overridden" };
     XCTAssertEqualObjects(labels.labelsDictionary, labels.customInfo);
+}
+
+- (void)testValues
+{
+    NSMutableArray *values = @[ @"value1", @"value2" ].mutableCopy;
+    SRGAnalyticsHiddenEventLabels *labels1 = [[SRGAnalyticsHiddenEventLabels alloc] init];
+    labels1.values = values;
+    
+    [values removeObjectAtIndex:0];
+    [values addObject:@"value1"];
+    
+    SRGAnalyticsHiddenEventLabels *labels1Copy = [labels1 copy];
+    labels1Copy.values = values;
+    XCTAssertNotEqualObjects(labels1, labels1Copy);
+    
+    SRGAnalyticsHiddenEventLabels *labels2 = [[SRGAnalyticsHiddenEventLabels alloc] init];
+    labels2.values = @[ @"value5", @"value4", @"value3", @"value2", @"value1" ];
+    NSDictionary *labelsDictionary = @{ @"event_value_1" : @"value5",
+                                        @"event_value_2" : @"value4",
+                                        @"event_value_3" : @"value3",
+                                        @"event_value_4" : @"value2",
+                                        @"event_value_5" : @"value1" };
+    XCTAssertEqualObjects(labels2.labelsDictionary, labelsDictionary);
+    
+    SRGAnalyticsHiddenEventLabels *labels3 = [[SRGAnalyticsHiddenEventLabels alloc] init];
+    labels3.values = @[];
+    XCTAssertEqual(labels3.labelsDictionary.count, 0);
 }
 
 @end
