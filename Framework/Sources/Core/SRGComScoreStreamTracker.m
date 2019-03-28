d50870d83a6cf5cba28d8e6d34dd0a7e6488d176
Samuel Défago <defagos@gmail.com>
2019-03-28 11:56:03 +0100

    Prepare tracker implementations

---
+++ Framework/Sources/Core/SRGComScoreStreamTracker.m
@@ -0,0 +1,11 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import "SRGComScoreStreamTracker.h"
+
+@implementation SRGComScoreStreamTracker
+
+@end
